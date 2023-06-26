4de1dca43169f49d6665b5c4a67c24d1ddd91160
Shelby Sanchez <shelby@bluearrowmail.com>
2023-06-26 16:38:34 -0400

    updated repo url

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export const creations = [
       id: 1,
       title: "Personal-Website",
       img: portfolio,
-      link: "https://github.com/realshelby/personal-website-v1",
+      link: "https://github.com/shelby-cobra/personal-website-v1",
       desc: "Portfolio built with React.js leveraging Material UI",
     }
   ];
